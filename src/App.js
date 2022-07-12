8dcd240808c6c5873155d3225051369347e005f2
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-12 08:42:33 -0700

    00-gave item unique id

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
 				<h3>Comments ({comments.length})</h3>
 				<ul>
 					{comments.map((comment, index) => (
-						<li>{comment.text}</li>
+						<li key={index}>{comment.text}</li>
 					))}
 					<li></li>
 				</ul>
