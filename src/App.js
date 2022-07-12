ddafc0cc73d7c510b536ba663f51fb0df54014c3
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-12 08:41:49 -0700

    00-mapping array to html

--- src/App.js
+++ src/App.js
@@ -3,10 +3,26 @@ import React from 'react'
 function App() {
 	const title = 'Blog post!'
 	const body = 'This is my blog post!'
+
+	const comments = [
+		{ id: 1, text: 'comment one' },
+		{ id: 2, text: 'comment two' },
+		{ id: 3, text: 'comment three' },
+	]
+
 	return (
 		<>
 			<h1>{title}</h1>
 			<p>{body}</p>
+			<div className='comments'>
+				<h3>Comments ({comments.length})</h3>
+				<ul>
+					{comments.map((comment, index) => (
+						<li>{comment.text}</li>
+					))}
+					<li></li>
+				</ul>
+			</div>
 		</>
 	)
 }
