202f61141a16360c3c2b1dbe161a8024bbede8a0
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-13 15:08:01 -0700

    01 conditional styling (this doesn't work) vs conditional classes

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,18 +1,10 @@
-import { useState } from "react";
 import Card from "./shared/Card";
 
 function FeedbackItem({ item }) {
-  const [reverse, setReverse] = useState(true)
-
-  const clickHandler = () => {
-    setReverse(!true)
-  }
-
   return (
-    <Card reverse={reverse}>
+    <Card reverse={false}>
       <div className="num-display">{item.rating}</div>
       <div className="text-display">{item.text}</div>
-      <button onClick={clickHandler}>toggle</button>
     </Card>
   );
 }
--- src/components/shared/Card.js
+++ src/components/shared/Card.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Card({ children, reverse }) {
-  return <div className={`card ${reverse ? 'reverse' : ''}`}> {children} </div>;
+  return (
+  <div 
+    className='card' 
+    style={{
+      backgroundColor: reverse ? 'rgba(0,0,0,0.4)' : '#fff',
+      color: reverse ? '#fff' : 'rgba(0,0,0,0.4)',
+    }}
+  > 
+    {children} 
+  </div>
+  )
 }
 
 export default Card;
