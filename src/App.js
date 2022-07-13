c053870185d421d5a2c1329036a6cda5005a91c7
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-13 16:33:47 -0700

    01-calculating stats 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 import Header from "./components/Header";
-import FeedbackItem from "./components/FeedbackItem";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
+import FeedbackStats from "./components/FeedbackStats";
 
 // global level state vs component level state
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
   const deleteFeedback = (id) => {
-    if (window.confirm("Are you sure you want to delete?")) {
-      // set feedback to new array filtering the item with the id that user clicked on
-      // keep all the items that DON"T match the id (clicked)
-      setFeedback(feedback.filter((item) => item.id !== id));
-    }
+    // if (window.confirm("Are you sure you want to delete?")) {
+    //   // set feedback to new array filtering the item with the id that user clicked on
+    //   // keep all the items that DON"T match the id (clicked)
+    //   setFeedback(feedback.filter((item) => item.id !== id));
+    // }
+    setFeedback(feedback.filter((item) => item.id !== id));
   };
   return (
     <>
@@ -21,6 +22,7 @@ function App() {
       <div className="container">
         <h1>My app</h1>
       </div>
+      <FeedbackStats feedback={feedback}></FeedbackStats>
       <FeedbackList
         handleDelete={deleteFeedback}
         feedback={feedback}
---
+++ src/components/FeedbackStats.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function FeedbackStats({ feedback }) {
+  let avg =
+    feedback.reduce((acc, cur) => {
+      return acc + cur.rating;
+    }, 0) / feedback.length;
+
+  return (
+    <div className="feedback-stats">
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Average Rating: {avg}</h4>
+    </div>
+  );
+}
+
+export default FeedbackStats;
