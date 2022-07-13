8f0a93632652401903539728fa9deaf015445291
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-13 16:34:58 -0700

    01- isNaN for dividing by zero

--- src/components/FeedbackStats.js
+++ src/components/FeedbackStats.js
@@ -9,7 +9,7 @@ function FeedbackStats({ feedback }) {
   return (
     <div className="feedback-stats">
       <h4>{feedback.length} Reviews</h4>
-      <h4>Average Rating: {avg}</h4>
+      <h4>Average Rating: {isNaN(avg) ? 0 : avg}</h4>
     </div>
   );
 }
