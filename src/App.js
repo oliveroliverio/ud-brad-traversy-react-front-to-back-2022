70d92c0567e7e6a1a489ef6acef435b66ff05e5a
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-13 14:26:09 -0700

    01 added data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState }  from 'react'
 import Header from './components/Header'
 import FeedbackItem from './components/FeedbackItem'
 
 // global level state vs component level state
 
 function App() {
+	const [feedback, setFeedback] = useState()
 	return (
 		<>
 			<Header text='hello world'></Header>
