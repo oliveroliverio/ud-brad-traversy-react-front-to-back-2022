52349844cc49f1abb4b0ea55df13741ebd821bec
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-13 16:10:15 -0700

    01 propdrilling upwards 1

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,17 +1,21 @@
+import { FaTimes } from "react-icons/fa";
 import Card from "./shared/Card";
-import PropTypes from 'prop-types'
-
-function FeedbackItem({ item }) {
+import PropTypes from "prop-types";
+// note this button is supposed to delete one of the items, but the items list isn't in this component, it's in the app component one level above.  We would need to forward upwards
+function FeedbackItem({ item, handleDelete }) {
   return (
     <Card reverse={false}>
       <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className="close">
+        <FaTimes color="purple"></FaTimes>
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
 }
 
 FeedbackItem.propTypes = {
-  item: PropTypes.object.isRequired
-}
+  item: PropTypes.object.isRequired,
+};
 
 export default FeedbackItem;
--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,21 +1,23 @@
 import React from "react";
-import FeedbackItem from "./FeedbackItem"
-import PropTypes from 'prop-types'
+import FeedbackItem from "./FeedbackItem";
+import PropTypes from "prop-types";
 
 function FeedbackList({ feedback }) {
-  // conditionally show feed back if empty case
-  // if no feedback or feed back is zero
-
-  // Note right now, data is hardcoded 
   if (!feedback || feedback.length === 0) {
-    return <p>No Feedback yet</p>
+    return <p>No Feedback yet</p>;
   }
 
-  return <div className='feedback-list'>
-    {feedback.map((i, index) => (
-      <FeedbackItem key={i.id} item={i}></FeedbackItem>
-    ))}
-  </div>;
+  return (
+    <div className="feedback-list">
+      {feedback.map((i, index) => (
+        <FeedbackItem
+          key={i.id}
+          item={i}
+          handleDelete={(id) => console.log(id)}
+        ></FeedbackItem>
+      ))}
+    </div>
+  );
 }
 
 FeedbackList.propTypes = {
@@ -24,9 +26,8 @@ FeedbackList.propTypes = {
       id: PropTypes.number.isRequired,
       text: PropTypes.string.isRequired,
       rating: PropTypes.number.isRequired,
-    
     })
-  )
-}
+  ),
+};
 
-export default FeedbackList
+export default FeedbackList;
