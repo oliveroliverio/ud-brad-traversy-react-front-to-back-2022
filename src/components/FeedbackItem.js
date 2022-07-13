12ec2366cf0b029adb9f1b0344a8afb4807b5c30
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-13 14:54:07 -0700

    01 refactoring a Cardcomponent

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,12 +1,12 @@
+import Card from "./shared/Card";
 
-function FeedbackItem({item}) {
-
+function FeedbackItem({ item }) {
   return (
-    <div className='card'>
+    <Card>
       <div className="num-display">{item.rating}</div>
       <div className="text-display">{item.text}</div>
-    </div>
-  )
+    </Card>
+  );
 }
 
-export default FeedbackItem
+export default FeedbackItem;
