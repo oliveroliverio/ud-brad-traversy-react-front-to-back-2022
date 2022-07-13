65e9bfbcfaf1a391e795eeea791af3b87eb66dad
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-13 14:20:46 -0700

    01 button onClick change rating

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -6,17 +6,13 @@ function FeedbackItem() {
   const [text, setText] = useState('This is an example of a feedback item text')
 
   const handleClick = () => {
-    // refactor so setRating takes in a function when need to know the previous state
-    setRating((prev) => {
-      return prev + 1
-    }) 
+    setRating(10)
   }
-
   return (
     <div className='card'>
       <div className="num-display">{rating}</div>
       <div className="text-display">{text}</div>
-      <button onClick={handleClick}>click</button>
+      <button onClick={handleClick} className="ui button">click</button>
     </div>
   )
 }
