83a7d1cbf22ef464c1289dcf5a7ec3e6d1f88b17
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-13 16:37:39 -0700

    01-remove trailing zeros and regex

--- src/components/FeedbackStats.js
+++ src/components/FeedbackStats.js
@@ -6,6 +6,11 @@ function FeedbackStats({ feedback }) {
       return acc + cur.rating;
     }, 0) / feedback.length;
 
+  // reformat the avg fixed to 1 decimal place
+  // avg = avg.toFixed(1);
+  // reformat the avg fixed to 1 decimal place, but remove trailing zeros
+  avg = avg.toFixed(1).replace(/[.,]0$/, "");
+
   return (
     <div className="feedback-stats">
       <h4>{feedback.length} Reviews</h4>
