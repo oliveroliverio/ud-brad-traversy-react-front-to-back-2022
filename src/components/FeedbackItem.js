fd5e72dbe3fe62f9e25ed6dd53e38a0bda3c78e6
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-13 15:01:20 -0700

    01 playing around with useState

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import Card from "./shared/Card";
 
 function FeedbackItem({ item }) {
+  const [reverse, setReverse] = useState(true)
+
+  const clickHandler = () => {
+    setReverse(!true)
+  }
+
   return (
-    <Card reverse={true}>
+    <Card reverse={reverse}>
       <div className="num-display">{item.rating}</div>
       <div className="text-display">{item.text}</div>
+      <button onClick={clickHandler}>toggle</button>
     </Card>
   );
 }
