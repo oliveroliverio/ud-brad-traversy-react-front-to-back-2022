b280f51b309909f7236a0949e3dc123e04232561
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-13 18:05:48 -0700

    01 animated adding and removing components

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,6 +1,7 @@
-import React from "react";
-import FeedbackItem from "./FeedbackItem";
-import PropTypes from "prop-types";
+import React from 'react';
+import FeedbackItem from './FeedbackItem';
+import PropTypes from 'prop-types';
+import { motion, AnimatePresence } from 'framer-motion';
 
 function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
@@ -8,16 +9,38 @@ function FeedbackList({ feedback, handleDelete }) {
   }
 
   return (
-    <div className="feedback-list">
-      {feedback.map((i, index) => (
-        <FeedbackItem
-          key={i.id}
-          item={i}
-          handleDelete={handleDelete}
-        ></FeedbackItem>
-      ))}
+    <div className='feedback-list'>
+      <AnimatePresence>
+        {feedback.map((i, index) => (
+          <motion.div
+            key={i.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={i.id}
+              item={i}
+              handleDelete={handleDelete}
+            ></FeedbackItem>
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
+
+  //Without animation
+  // return (
+  //   <div className='feedback-list'>
+  //     {feedback.map((i, index) => (
+  //       <FeedbackItem
+  //         key={i.id}
+  //         item={i}
+  //         handleDelete={handleDelete}
+  //       ></FeedbackItem>
+  //     ))}
+  //   </div>
+  // );
 }
 
 FeedbackList.propTypes = {
