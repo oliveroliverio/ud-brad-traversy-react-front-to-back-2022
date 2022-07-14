86e4520e55db1f70a9af761e214f420fb1d425c6
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-13 17:04:38 -0700

    01 - useState, changing input text state, instead of console logging, calling setState on it

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./shared/Card";
-
+// for each input in form, you need state
 function FeedbackForm() {
+  const [text, setText] = useState("");
+
+  // set as arrow function that takes in an event paramter
+  const handleTextChange = (e) => {
+    console.log(e.target.value);
+  };
   return (
     <Card reverse={false}>
       <form>
         <h2>How would you rate your service with us?</h2>
         {/* @todo - rating select component */}
         <div className="input-group">
-          <input type="text" placeholder="Write a review" />
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="Write a review"
+          />
           <button className="ui button" type="submit">
             Send
           </button>
