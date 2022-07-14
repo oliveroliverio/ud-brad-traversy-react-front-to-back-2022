5e09aa86892d81a3cc9b10837831f14cb596bc6e
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-14 13:58:18 -0700

    01 added default data file in FeedbackContext

--- src/App.js
+++ src/App.js
@@ -7,10 +7,8 @@ import {
 } from 'react-router-dom';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
-import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
-
 import AboutPage from './pages/AboutPage';
 import { FeedbackProvider } from './context/FeedbackContext';
 import AboutIconLink from './components/AboutIconLink';
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,18 +1,13 @@
 import { createContext, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import FeedbackData from '../data/FeedbackData';
 
 const FeedbackContext = createContext();
 
 // need to create a providor and wrap all the prop passing into it similar to how we wrapped everything in Router
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'default item from context',
-      rating: 10,
-    },
-  ]);
+  const [feedback, setFeedback] = useState(FeedbackData);
 
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
