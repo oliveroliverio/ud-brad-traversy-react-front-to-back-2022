d2486271edfc3fbe8a082ac0e5a7537c29ec8f70
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-15 16:48:14 -0700

    testing if can access .env variables from anywhere

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,9 @@
 function Home() {
-  return <h1 className='text-6xl'>Welcome</h1>
+  return (
+    <>
+      <h1 className='text-6xl'>Welcome</h1>
+      {process.env.REACT_APP_REACT_GITHUB_TOKEN}
+    </>
+  )
 }
 export default Home
