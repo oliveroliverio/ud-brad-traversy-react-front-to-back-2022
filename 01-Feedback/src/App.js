682919438d544d346cacc9c35fefb46b92606fb8
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-15 14:34:15 -0700

    02 init

---
+++ 01-Feedback/src/App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+} from 'react-router-dom';
+import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackStats from './components/FeedbackStats';
+import FeedbackForm from './components/FeedbackForm';
+import AboutPage from './pages/AboutPage';
+import { FeedbackProvider } from './context/FeedbackContext';
+import AboutIconLink from './components/AboutIconLink';
+import Card from './components/shared/Card';
+// import Post from './components/Post';
+
+// yarn run build
+
+function App() {
+  return (
+    <FeedbackProvider>
+      <Router>
+        <Header text='hello world'></Header>
+        <div className='container'>
+          <Routes>
+            <Route
+              exact
+              path='/'
+              element={
+                <>
+                  <FeedbackForm></FeedbackForm>
+                  <FeedbackStats></FeedbackStats>
+                  <FeedbackList />
+                </>
+              }
+            ></Route>
+            <Route path='/about' element={<AboutPage />}>
+              About Page
+            </Route>
+            {/* <Route path='/post/*' element={<Post />}>
+            Post
+          </Route> */}
+          </Routes>
+        </div>
+        <AboutIconLink></AboutIconLink>
+        <Card>
+          <NavLink to='/' activeClassName='active'>
+            Home
+          </NavLink>
+          <NavLink to='/about' activeClassName='active'>
+            About
+          </NavLink>
+        </Card>
+      </Router>
+    </FeedbackProvider>
+  );
+}
+export default App;
