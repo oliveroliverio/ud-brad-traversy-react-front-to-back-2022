3949f6c3b3b4d4bded052a7fa2ba67a0f1a6be14
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-15 13:26:01 -0700

    feedback added to server persisting and db.json entry added.

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,5 +1,4 @@
 import { createContext, useState, useEffect } from 'react'
-import { v4 as uuidv4 } from 'uuid'
 // don't need this anymore, going to get from json-server
 // import FeedbackData from '../data/FeedbackData'
 
@@ -23,9 +22,17 @@ export const FeedbackProvider = ({ children }) => {
     setIsLoading(false)
   }
 
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newFeedback),
+    })
+
+    const data = await response.json()
+    setFeedback([data, ...feedback])
   }
 
   const [feedbackEditState, setFeedbackEditState] = useState({
