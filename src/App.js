1c0f0fcd3e50cabf1d1a8393120e59e80dde2f31
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-15 14:53:41 -0700

    correctly installed tailwind and daisyUI and tested

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from 'react'
 
-function App(){
+function App() {
   return (
-  	<>
-    	<h1>Boiler Plate Code Removed</h1>
-    </>
+    <div className='bg-purple-500'>
+      <h1 className='text-xl'>Hello World</h1>
+      <button className='btn'>Click</button>
+    </div>
   )
 }
-export default App;
+export default App
