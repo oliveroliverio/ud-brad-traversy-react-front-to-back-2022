bc4f3cff1a3289eecc705ec73ec0b2dbfba29038
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-14 22:12:20 -0700

    useEffect, sideEffect, visually showing correct rating

--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -1,19 +1,19 @@
 const FeedbackData = [
-  {
-    id: 1,
-    rating: 10,
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
-  },
-  {
-    id: 2,
-    rating: 22,
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
-  },
-  {
-    id: 3,
-    rating: 5.5,
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
-  },
-];
+	{
+		id: 1,
+		rating: 10,
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+	},
+	{
+		id: 2,
+		rating: 7,
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+	},
+	{
+		id: 3,
+		rating: 3,
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+	},
+]
 
-export default FeedbackData;
+export default FeedbackData
