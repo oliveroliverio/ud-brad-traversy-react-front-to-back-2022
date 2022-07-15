49d0ff196fe5fac486c52962903c9a88c37b4b1f
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-15 15:10:05 -0700

    02 container mx-auto giving some margin in navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,7 +5,8 @@ import PropTypes from 'prop-types'
 function Navbar({ title }) {
   return (
     <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
-      Navbar
+      {/* auto margin left and right */}
+      <div className='container mx-auto'>Navbar</div>
     </nav>
   )
 }
