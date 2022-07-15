d17c4938904d1dc3564256963fa23d3f33a69a02
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-14 21:48:14 -0700

    added editFeedback to context, look at chromedev tools -> components -> feedbackprovier -> click on edit icon and see state change

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,24 +1,27 @@
-import { FaTimes } from 'react-icons/fa';
-import Card from './shared/Card';
-import PropTypes from 'prop-types';
-import { useContext } from 'react';
-import FeedbackContext from '../context/FeedbackContext';
+import { FaTimes, FaEdit } from 'react-icons/fa'
+import Card from './shared/Card'
+import PropTypes from 'prop-types'
+import { useContext } from 'react'
+import FeedbackContext from '../context/FeedbackContext'
 // note this button is supposed to delete one of the items, but the items list isn't in this component, it's in the app component one level above.  We would need to forward upwards
 function FeedbackItem({ item }) {
-  const { deleteFeedback } = useContext(FeedbackContext);
-  return (
-    <Card reverse={false}>
-      <div className='num-display'>{item.rating}</div>
-      <button onClick={() => deleteFeedback(item.id)} className='close'>
-        <FaTimes color='purple'></FaTimes>
-      </button>
-      <div className='text-display'>{item.text}</div>
-    </Card>
-  );
+	const { deleteFeedback, editFeedback } = useContext(FeedbackContext)
+	return (
+		<Card reverse={false}>
+			<div className='num-display'>{item.rating}</div>
+			<button onClick={() => deleteFeedback(item.id)} className='close'>
+				<FaTimes color='purple'></FaTimes>
+			</button>
+			<button onClick={() => editFeedback(item)} className='edit'>
+				<FaEdit color='purple' />
+			</button>
+			<div className='text-display'>{item.text}</div>
+		</Card>
+	)
 }
 
 FeedbackItem.propTypes = {
-  item: PropTypes.object.isRequired,
-};
+	item: PropTypes.object.isRequired,
+}
 
-export default FeedbackItem;
+export default FeedbackItem
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,35 +1,49 @@
-import { createContext, useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import FeedbackData from '../data/FeedbackData';
+import { createContext, useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import FeedbackData from '../data/FeedbackData'
 
-const FeedbackContext = createContext();
+const FeedbackContext = createContext()
 
 // need to create a providor and wrap all the prop passing into it similar to how we wrapped everything in Router
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState(FeedbackData);
+	const [feedback, setFeedback] = useState(FeedbackData)
 
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
-  };
+	const addFeedback = (newFeedback) => {
+		newFeedback.id = uuidv4()
+		setFeedback([newFeedback, ...feedback])
+	}
 
-  const deleteFeedback = (id) => {
-    setFeedback(feedback.filter((item) => item.id !== id));
-  };
-  return (
-    <FeedbackContext.Provider
-      value={{
-        feedback,
-        deleteFeedback,
-        addFeedback,
-      }}
-    >
-      {/* chilren are the components we wrap around FeedbackProvider 
+	const [feedbackEdit, setFeedbackEdit] = useState({
+		item: {},
+		// by default, false, if clicked, set to true (edit mode)
+		edit: false,
+	})
+
+	const deleteFeedback = (id) => {
+		setFeedback(feedback.filter((item) => item.id !== id))
+	}
+
+	// set item to be updated
+	const editFeedback = (item) => {
+		setFeedbackEdit({
+			item,
+			edit: true,
+		})
+	}
+	return (
+		<FeedbackContext.Provider
+			value={{
+				feedback,
+				deleteFeedback,
+				addFeedback,
+				editFeedback,
+			}}>
+			{/* chilren are the components we wrap around FeedbackProvider
      Any values like state or fucntions to use (used to be props) would be in value*/}
-      {children}
-    </FeedbackContext.Provider>
-  );
-};
+			{children}
+		</FeedbackContext.Provider>
+	)
+}
 
-export default FeedbackContext;
+export default FeedbackContext
