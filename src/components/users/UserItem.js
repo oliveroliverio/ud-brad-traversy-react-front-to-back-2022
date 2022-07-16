9c3960307aa078671250bfdc77e2657f2694bae8
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-16 11:29:14 -0700

    UserItem subdiv: lex-row items-center space-x-4 card-body

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,7 +1,11 @@
 import PropTypes from 'prop-types'
 function UserItem({ user: { login, avatar_url } }) {
   //
-  return <div className='card shadow-md compact side bg-base-100'>{login}</div>
+  return (
+    <div className='card shadow-md compact side bg-base-100'>
+      <div className='flex-row items-center space-x-4 card-body'></div>
+    </div>
+  )
 }
 
 UserItem.propTypes = {
