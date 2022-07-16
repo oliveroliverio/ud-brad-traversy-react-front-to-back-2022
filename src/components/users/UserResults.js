bd9798c23f6e38a515508d3cc169381ea5462738
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-15 19:50:10 -0700

    rendering github uers in UserResults component

--- src/components/users/UserResults.js
+++ src/components/users/UserResults.js
@@ -17,9 +17,16 @@ function UserResults() {
 
     const data = await response.json()
     setUsers(data)
-    console.log(users)
+    // console.log(users)
     setLoading(false)
   }
-  return <div>UserResults</div>
+
+  return (
+    <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+      {users.map((user) => (
+        <h3>{user.login}</h3>
+      ))}
+    </div>
+  )
 }
 export default UserResults
