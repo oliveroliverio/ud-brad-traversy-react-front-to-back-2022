1e97aad66311c65ae4bce76a3ac8c2ab9d06ef51
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-16 12:22:33 -0700

    refactored UserResults and transfered to GithubContext

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,13 +1,14 @@
 import { createContext, useState } from 'react'
 
-const GithubContext = createContext()
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEMN
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+const GithubContext = createContext()
 
 export const GithubProvider = ({ children }) => {
   const [users, setUsers] = useState([])
   const [loading, setLoading] = useState(true)
 
+  // create fetchUsers async/await function to run within useEffect
   const fetchUsers = async () => {
     const response = await fetch(`${GITHUB_URL}/users`, {
       headers: {
@@ -17,12 +18,11 @@ export const GithubProvider = ({ children }) => {
 
     const data = await response.json()
     setUsers(data)
-    // console.log(users)
     setLoading(false)
   }
 
   return (
-    <GithubContext.Provider value={(users, loading, fetchUsers)}>
+    <GithubContext.Provider value={{ users, loading, fetchUsers }}>
       {children}
     </GithubContext.Provider>
   )
