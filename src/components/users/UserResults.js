ef279027a66eefad080e319d1a273bae5dae4032
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-15 17:00:18 -0700

    created fetchUsers async function to be used in useEffect (when component first loads)

--- src/components/users/UserResults.js
+++ src/components/users/UserResults.js
@@ -3,6 +3,18 @@ import { useEffect } from 'react'
 function UserResults() {
   // want this to execute right when component loads
   useEffect(() => {}, [])
+
+  // create fetchUsers async/await function to run within useEffect
+  const fetchUsers = async () => {
+    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+      },
+    })
+
+    const data = await response.json()
+    console.log(data)
+  }
   return <div>UserResults</div>
 }
 export default UserResults
