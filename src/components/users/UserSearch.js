1088816e20b59ee3fdd6d5b188da8665d06e438e
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-16 16:47:14 -0700

    added interface items: buttons, etc

--- src/components/users/UserSearch.js
+++ src/components/users/UserSearch.js
@@ -1,7 +1,35 @@
 import React from 'react'
 
 function UserSearch() {
-	return <div>Search</div>
+	// in grid: search form + button -> one item. then clear button
+	// grid-cols-1: small screen
+	return (
+		<div className='grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8'>
+			{/* Div containing form */}
+			<div>
+				<form action=''>
+					<div className='form-control'>
+						<div className='relative'>
+							<input
+								type='text'
+								className='w-full pr-40 bg-gray-200 input input-lg text-black'
+								placeholder='search'
+							/>
+							<button
+								type='submit'
+								className='absolute top-0 right-0 rounded-l-none w-36 btn btn-lg'>
+								Go
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+			{/* Div containing clear button */}
+			<div>
+				<button className='btn btn-ghost btn-large'>Clear</button>
+			</div>
+		</div>
+	)
 }
 
 export default UserSearch
