6833167dc7a349497de85ac47269f3119714a1e9
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-16 11:32:04 -0700

    styling avatar and login name

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -3,7 +3,14 @@ function UserItem({ user: { login, avatar_url } }) {
   //
   return (
     <div className='card shadow-md compact side bg-base-100'>
-      <div className='flex-row items-center space-x-4 card-body'></div>
+      <div className='flex-row items-center space-x-4 card-body'>
+        <div className='avatar'>
+          <div className='rounded-full shadow w-14 h-14'>
+            <img src={avatar_url} alt='profile' />
+          </div>
+        </div>
+        <div className='card-title'>{login}</div>
+      </div>
     </div>
   )
 }
