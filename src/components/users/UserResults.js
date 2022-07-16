a9edb65f621404f979ef31df2bfce46ed85c1f77
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-15 19:53:26 -0700

    added loading condition

--- src/components/users/UserResults.js
+++ src/components/users/UserResults.js
@@ -21,12 +21,16 @@ function UserResults() {
     setLoading(false)
   }
 
-  return (
-    <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
-      {users.map((user) => (
-        <h3>{user.login}</h3>
-      ))}
-    </div>
-  )
+  if (!loading) {
+    return (
+      <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+        {users.map((user) => (
+          <h3>{user.login}</h3>
+        ))}
+      </div>
+    )
+  } else {
+    return <h3>Loading...</h3>
+  }
 }
 export default UserResults
