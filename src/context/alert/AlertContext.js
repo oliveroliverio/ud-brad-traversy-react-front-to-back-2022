d3755d7131da7ffc2bef3a35c1fb9e2e1f5a5e3f
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-17 13:43:52 -0700

    create setAlert function in AlertContext

--- src/context/alert/AlertContext.js
+++ src/context/alert/AlertContext.js
@@ -8,10 +8,22 @@ export const AlertProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(AlertReducer, initialState)
 
+  // after checking to see that context is "hooked up" by going to dev tools-> components
+  // create the alert function below
+  const setAlert = (msg, type) => {
+    dispatch({
+      type: 'SET_ALERT',
+      payload: { msg, type },
+    })
+    // want this to go away after a few seconds
+    setTimeout(() => dispatch({ type: 'REMOVE_ALERT' }), 3000)
+  }
+
   return (
     <AlertContext.Provider
       values={{
         alert: state,
+        setAlert,
       }}
     >
       {children}
