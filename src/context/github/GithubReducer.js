e8abf07bc0dd45ececb88c2e78ad032db132f136
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-17 14:59:46 -0700

    Create action in GithubReducer to implement the GithubContext GET_USER action

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -17,6 +17,12 @@ const githubReducer = (state, action) => {
         users: [],
       }
     }
+    case 'GET_USER':
+      return {
+        ...state,
+        user: action.payload,
+        loading: false,
+      }
     default:
       return state
   }
