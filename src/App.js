fd696d5694982f4819f4051a9e7f565b69a0cb83
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-17 13:38:55 -0700

    Wrap AlertProvider around everything within Router in App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
 import { GithubProvider } from './context/github/GithubContext'
+import { AlertProvider } from './context/alert/AlertContext'
 
 // create environment variable accessible anywhere (github API token)
 
@@ -15,22 +16,24 @@ function App() {
       flex column, // justify-between: align space in between each thing
       (navbar, main content, footer) // h-screen: take up entire screen height
       vertically */}
-      <Router>
-        <div className='flex flex-col justify-between h-screen'>
-          <Navbar />
-          {/* have main content in a container so it's not all the way to the side */}
-          {/* emmet: main.container.mx-auto.px-3.pb-12 */}
-          <main className='container mx-auto px-3 pb-12'>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/notfound' element={<NotFound />} />
-              <Route path='/*' element={<NotFound />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
+      <AlertProvider>
+        <Router>
+          <div className='flex flex-col justify-between h-screen'>
+            <Navbar />
+            {/* have main content in a container so it's not all the way to the side */}
+            {/* emmet: main.container.mx-auto.px-3.pb-12 */}
+            <main className='container mx-auto px-3 pb-12'>
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/notfound' element={<NotFound />} />
+                <Route path='/*' element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   )
 }
