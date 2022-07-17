328e684d9daf1c41edb0e261986ea831728a99b7
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-16 18:30:42 -0700

    implemented search users

--- src/components/users/UserSearch.js
+++ src/components/users/UserSearch.js
@@ -3,56 +3,57 @@ import GithubContext from '../../context/github/GithubContext'
 import React from 'react'
 
 function UserSearch() {
-	const [text, setText] = useState('')
+  const [text, setText] = useState('')
 
-	const { users } = useContext(GithubContext)
+  const { users, searchUsers } = useContext(GithubContext)
 
-	const handleChange = (e) => setText(e.target.value)
-	const handleSubmit = (e) => {
-		e.preventDefault()
-		// validation
-		if (text === '') {
-			alert('please enter something')
-		} else {
-			// @todo - search users
+  const handleChange = (e) => setText(e.target.value)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // validation
+    if (text === '') {
+      alert('please enter something')
+    } else {
+      // @todo - search users
+      searchUsers(text)
+      setText('')
+    }
+  }
 
-			setText('')
-		}
-	}
-
-	return (
-		// in grid: search form + button -> one item. then clear button
-		// grid-cols-1: small screen
-		<div className='grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8'>
-			{/* Div containing form */}
-			<div>
-				<form onSubmit={handleSubmit}>
-					<div className='form-control'>
-						<div className='relative'>
-							<input
-								type='text'
-								className='w-full pr-40 bg-gray-200 input input-lg text-black'
-								placeholder='search'
-								value={text}
-								onChange={handleChange}
-							/>
-							<button
-								type='submit'
-								className='absolute top-0 right-0 rounded-l-none w-36 btn btn-lg'>
-								Go
-							</button>
-						</div>
-					</div>
-				</form>
-			</div>
-			{/* this came from Github context */}
-			{users.length > 0 && (
-				<div>
-					<button className='btn btn-ghost btn-large'>Clear</button>
-				</div>
-			)}
-		</div>
-	)
+  return (
+    // in grid: search form + button -> one item. then clear button
+    // grid-cols-1: small screen
+    <div className='grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8'>
+      {/* Div containing form */}
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <div className='relative'>
+              <input
+                type='text'
+                className='w-full pr-40 bg-gray-200 input input-lg text-black'
+                placeholder='search'
+                value={text}
+                onChange={handleChange}
+              />
+              <button
+                type='submit'
+                className='absolute top-0 right-0 rounded-l-none w-36 btn btn-lg'
+              >
+                Go
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+      {/* this came from Github context */}
+      {users.length > 0 && (
+        <div>
+          <button className='btn btn-ghost btn-large'>Clear</button>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default UserSearch
