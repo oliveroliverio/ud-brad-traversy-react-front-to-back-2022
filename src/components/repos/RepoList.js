ea7ca82e9765606fce62dd12ac3f654db128d3de
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-18 16:12:19 -0700

    added daisyUI styling

--- src/components/repos/RepoList.js
+++ src/components/repos/RepoList.js
@@ -1,15 +1,26 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import RepoItem from './RepoItem'
 
 function RepoList({ repos }) {
   return (
     <>
-      <div>RepoLIst</div>
-      {repos.map((i) => {
-        return <div>{i.name}</div>
-      })}
+      <div className='rounded-lg shadow-lg card bg-base-100'>
+        <div className='card-body'>
+          <h2 className='text-3xl my-4 font-bold card-title'>
+            Top Repositories
+          </h2>
+          {repos.map((i) => (
+            <h3>{i.name}</h3>
+          ))}
+        </div>
+      </div>
     </>
   )
+}
+
+RepoList.propTypes = {
+  repos: PropTypes.array.isRequired,
 }
 
 export default RepoList
