9df51b1b0b0ca37d0b7994cef8fadf66f958bdb6
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-18 13:11:25 -0700

    Get relevant stuff from GithubContext, react-icons, user variables, and markup the User page

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,20 +1,59 @@
+import { FaCodepen, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa'
 import { useEffect, useContext } from 'react'
 import GithubContext from '../context/github/GithubContext'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
+import Spinner from '../components/layout/Spinner'
 
 import React from 'react'
 
 function User() {
-  const { getUser, user } = useContext(GithubContext)
+  const { getUser, user, loading } = useContext(GithubContext)
 
   const params = useParams()
 
   useEffect(() => {
     getUser(params.login)
   }, [])
+
+  const {
+    name,
+    type,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    twitter_username,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+  } = user
+
+  if (loading) return <Spinner />
+
   return (
     <>
-      <div>{user.login}</div>
+      {/* Wrapper div */}
+      <div className='w-full mx-auto lg:w-10/12'>
+        {/* Global back link to go back to search page */}
+        <div className='mb-4'>
+          <Link to='/' className='btn btn-ghost'>
+            Back to search
+          </Link>
+        </div>
+        <div className='grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8'>
+          <div className='custom-card-image mb-6 md:mb-0'>
+            <div className='rounded-lg shadow-xl card image-fulll'>
+              <figure>
+                <img src={avatar_url} alt='pic' />
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
