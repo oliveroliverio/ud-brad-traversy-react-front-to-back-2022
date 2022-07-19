acdfd2e2033ae440d9af7b29350a29f6fdb0da77
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-19 15:07:02 -0700

    not using key prop in this

--- src/components/repos/RepoItem.js
+++ src/components/repos/RepoItem.js
@@ -1,6 +1,6 @@
 import { FaEye, FaInfo, FaLink, FaStar, FaUtensils } from 'react-icons/fa'
 import PropTypes from 'prop-types'
-function RepoItem({ key, repo }) {
+function RepoItem({ repo }) {
   const {
     name,
     description,
