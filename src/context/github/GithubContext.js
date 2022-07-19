b045a778dd1283c83e7b7793ba83301dc7b9b845
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-19 14:47:51 -0700

    GithubContext Cleaned

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,8 +1,6 @@
 import { createContext, useReducer } from 'react'
 import githubReducer from './GithubReducer'
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 const GithubContext = createContext()
 
 export const GithubProvider = ({ children }) => {
@@ -15,69 +13,11 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState)
 
-  // get singleUser
-  const getUser = async (login) => {
-    setLoading()
-
-    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    })
-
-    if (response.status === 404) {
-      window.location = '/notfound'
-    } else {
-      const data = await response.json()
-
-      dispatch({
-        type: 'GET_USER',
-        payload: data,
-      })
-    }
-  }
-
-  const clearUsers = () => {
-    dispatch({ type: 'CLEAR_USERS' })
-  }
-
-  // create setLoading function since we'll use this in a few different places
-  const setLoading = () => dispatch({ type: 'SET_LOADING' })
-
-  // get user Repos
-  const getUserRepos = async (login) => {
-    setLoading()
-
-    const params = new URLSearchParams({
-      sort: 'created',
-      per_page: 10,
-    })
-
-    const response = await fetch(
-      `${GITHUB_URL}/users/${login}/repos?${params}`,
-      {
-        headers: {
-          Authorization: `token ${GITHUB_TOKEN}`,
-        },
-      }
-    )
-
-    const data = await response.json()
-
-    dispatch({
-      type: 'GET_REPOS',
-      payload: data,
-    })
-  }
-
   return (
     <GithubContext.Provider
       value={{
         ...state,
         dispatch,
-        getUser,
-        getUserRepos,
-        clearUsers,
       }}
     >
       {children}
