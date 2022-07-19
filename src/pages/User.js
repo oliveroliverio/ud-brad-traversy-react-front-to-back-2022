87d86a22ec8930bd95f171fa4f6d13b3f8a9e53f
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-19 15:00:19 -0700

    moved getUser to User file and cleaned up GithubContext

--- src/pages/User.js
+++ src/pages/User.js
@@ -4,18 +4,25 @@ import GithubContext from '../context/github/GithubContext'
 import { useParams, Link } from 'react-router-dom'
 import Spinner from '../components/layout/Spinner'
 import RepoList from '../components/repos/RepoList'
+import { getUser, getUserRepos } from '../context/github/GithubActions'
 
 import React from 'react'
 
 function User() {
-  const { getUser, user, loading, getUserRepos, repos } =
-    useContext(GithubContext)
+  const { user, loading, repos, dispatch } = useContext(GithubContext)
 
   const params = useParams()
 
   useEffect(() => {
-    getUser(params.login)
-    getUserRepos(params.login)
+    dispatch({ type: 'SET_LOADING' })
+    const getUserData = async () => {
+      const userData = await getUser(params.login)
+      dispatch({ type: 'GET_USER', payload: userData })
+
+      const userRepoData = await getUserRepos(params.login)
+      dispatch({ type: 'GET_REPOS', payload: userRepoData })
+    }
+    getUserData()
   }, [])
 
   const {
