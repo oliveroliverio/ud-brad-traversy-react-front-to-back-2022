aa28ebcf68e973115ee0632b38129526a3249abd
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-18 18:16:33 -0700

    added URLSearchParams for Repo listing

--- src/components/repos/RepoList.js
+++ src/components/repos/RepoList.js
@@ -8,7 +8,7 @@ function RepoList({ repos }) {
       <div className='rounded-lg shadow-lg card bg-base-100'>
         <div className='card-body'>
           <h2 className='text-3xl my-4 font-bold card-title'>
-            Top Repositories
+            Latest Repositories
           </h2>
           {repos.map((i) => (
             <h3>{i.name}</h3>
