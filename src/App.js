c1a7db19009eead1e0a985ebcb66a593685cc307
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-22 15:45:58 -0700

    import BrowserRouter and pages in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-// create environment variable accessible anywhere (github API token)
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Explore from './pages/Explore'
+import Offers from './pages/Offers'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import ForgotPassword from './pages/ForgotPassword'
 
 function App() {
   return <>Home</>
