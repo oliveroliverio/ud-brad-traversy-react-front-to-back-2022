874771989eab5b8cec832e407a9fae254596a417
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-22 15:49:32 -0700

    added routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Explore from './pages/Explore'
 import Offers from './pages/Offers'
 import Profile from './pages/Profile'
@@ -7,6 +7,20 @@ import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
 
 function App() {
-  return <>Home</>
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<SignIn />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+      {/* NavBar */}
+    </>
+  )
 }
 export default App
