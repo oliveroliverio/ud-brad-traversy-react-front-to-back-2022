afc1f95bda661f013390bc9f26ed1ea1425b9c56
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-23 16:11:55 -0700

    create Navbar component and import Icons, useNavigate, useLocation

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import { useNavigate, useLocation } from 'react-router-dom'
+import { ReactComponent as OfferIcon } from '../assets/svg/localOfferIcon.svg'
+import { ReactComponent as ExploreIcon } from '../assets/svg/ExploreIcon.svg'
+import { ReactComponent as PersonOutlineIcon } from '../assets/svg/PersonOutlineIcon.svg'
+
+function Navbar() {
+  return <div></div>
+}
+
+export default Navbar
