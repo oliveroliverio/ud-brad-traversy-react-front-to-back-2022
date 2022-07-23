3fe5935221e93a5da25b38736bdfd265f874aa44
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-23 16:22:00 -0700

    added onClick attributes to li items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,20 +4,23 @@ import { ReactComponent as ExploreIcon } from '../assets/svg/exploreIcon.svg'
 import { ReactComponent as PersonOutlineIcon } from '../assets/svg/personOutlineIcon.svg'
 
 function Navbar() {
+  const navigate = useNavigate()
+  const location = useLocation()
+
   return (
     // color of icon will vary depending on if use is on page
     <footer className='navbar'>
       <nav className='navbarNav'>
         <ul className='navbarListItems'>
-          <li className='navbarListItem'>
+          <li className='navbarListItem' onClick={() => navigate('/')}>
             <ExploreIcon fill='#2c2c2c' width='36px' height='36px' />
             <p>Explore</p>
           </li>
-          <li className='navbarListItem'>
+          <li className='navbarListItem' onClick={() => navigate('/offers')}>
             <OfferIcon fill='#2c2c2c' width='36px' height='36px' />
             <p>Offer</p>
           </li>
-          <li className='navbarListItem'>
+          <li className='navbarListItem' onClick={() => navigate('/profile')}>
             <PersonOutlineIcon fill='#2c2c2c' width='36px' height='36px' />
             <p>Profile</p>
           </li>
