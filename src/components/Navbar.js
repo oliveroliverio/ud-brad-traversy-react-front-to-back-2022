a710df48bd08b9177a3d42f24e22fefae8dc0567
Oliver Oliverio <oliveroliverio@gmail.com>
2022-07-23 16:27:54 -0700

    added conditional coloring depending on what page user is on

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,21 +7,41 @@ function Navbar() {
   const navigate = useNavigate()
   const location = useLocation()
 
+  // dynamically change icon color depending on what page user is on
+  const pathMatchRoute = (route) => {
+    // if user is on localhost:3000/profile then location.pathname equals that
+    if (route == location.pathname) {
+      return true
+    }
+  }
+
   return (
     // color of icon will vary depending on if use is on page
     <footer className='navbar'>
       <nav className='navbarNav'>
         <ul className='navbarListItems'>
           <li className='navbarListItem' onClick={() => navigate('/')}>
-            <ExploreIcon fill='#2c2c2c' width='36px' height='36px' />
+            <ExploreIcon
+              fill={pathMatchRoute('/') ? '#2c2c2c' : '#8f8f8f'}
+              width='36px'
+              height='36px'
+            />
             <p>Explore</p>
           </li>
           <li className='navbarListItem' onClick={() => navigate('/offers')}>
-            <OfferIcon fill='#2c2c2c' width='36px' height='36px' />
+            <OfferIcon
+              fill={pathMatchRoute('/offers') ? '#2c2c2c' : '#8f8f8f'}
+              width='36px'
+              height='36px'
+            />
             <p>Offer</p>
           </li>
           <li className='navbarListItem' onClick={() => navigate('/profile')}>
-            <PersonOutlineIcon fill='#2c2c2c' width='36px' height='36px' />
+            <PersonOutlineIcon
+              fill={pathMatchRoute('/profile') ? '#2c2c2c' : '#8f8f8f'}
+              width='36px'
+              height='36px'
+            />
             <p>Profile</p>
           </li>
         </ul>
